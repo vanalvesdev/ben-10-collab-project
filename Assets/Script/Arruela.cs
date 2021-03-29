70659cce3e4bfd6558a11fcd963c4176d97b7292
Vanderson Alves <vanalvesdev@gmail.com>
2021-03-29 10:04:49 -0300

    first commit

---
+++ Assets/Script/Arruela.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arruela : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            animator.SetTrigger("left");
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animator.SetTrigger("right");
+        }
+    }
+
+}
---
+++ Assets/Script/Treco2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treco2 : MonoBehaviour
+{
+    public GameObject treco2;
+    public GameObject fundo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ActiveTreco2()
+    {
+        treco2.SetActive(true);
+        fundo.SetActive(true);
+    }
+}
