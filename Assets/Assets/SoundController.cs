dc527b1f01bc7d2a41ad35a2b6f3080357c21b5e
Vanderson Alves <vanalvesdev@gmail.com>
2021-05-02 03:14:00 -0300

    fix audio

--- Assets/Assets/SoundController.cs
+++ Assets/Assets/SoundController.cs
@@ -1,18 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
     // Start is called before the first frame update
+    AudioSource audioSource;
 
     void Awake()
     {
         DontDestroyOnLoad(transform.gameObject);
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Start()
     {
+        DestroyAnotherObjects();
+        audioSource.Play();
+    }
+
+    private void DestroyAnotherObjects()
+    {
+       GameObject[] objects = GameObject.FindGameObjectsWithTag("SoundController");
+        if (objects.Length == 1) return;
+
+        if (!objects.Equals(gameObject))
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
